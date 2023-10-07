2a4c82c0ddefb42c14aeadc056c6e3bcca77ed24
Arianrhod Sandlot <theguidanceofawhitetower@gmail.com>
2023-10-07 18:21:26 +0800

    test: move tests to 2 groups

---
+++ tests/e2e/static.spec.ts
@@ -0,0 +1,47 @@
+import { expect, test } from '@playwright/test'
+
+test.describe('static methods', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/tests/e2e/test-page/')
+  })
+
+  test('launch nes', async ({ page }) => {
+    const canvas = page.locator('#canvas')
+    await expect(canvas).not.toBeAttached()
+
+    await page.getByText('nes', { exact: true }).click()
+    await page.waitForLoadState('networkidle')
+    await page.waitForTimeout(500)
+    await expect(canvas).toHaveScreenshot('launch-nes.png')
+  })
+
+  test('launch megadrive', async ({ page }) => {
+    const canvas = page.locator('#canvas')
+    await expect(canvas).not.toBeAttached()
+
+    await page.getByText('megadrive', { exact: true }).click()
+    await page.waitForLoadState('networkidle')
+    await page.waitForTimeout(500)
+    await expect(canvas).toHaveScreenshot('launch-megadrive.png')
+  })
+
+  test('launch gbc', async ({ page }) => {
+    const canvas = page.locator('#canvas')
+    await expect(canvas).not.toBeAttached()
+
+    await page.getByText('gbc', { exact: true }).click()
+    await page.waitForLoadState('networkidle')
+    await page.waitForTimeout(500)
+    await expect(canvas).toHaveScreenshot('launch-gbc.png')
+  })
+
+  test('launch nestopia', async ({ page }) => {
+    const canvas = page.locator('#canvas')
+    await expect(canvas).not.toBeAttached()
+
+    await page.getByText('launchNestopia', { exact: true }).click()
+    await page.waitForLoadState('networkidle')
+    await page.waitForTimeout(500)
+    await expect(canvas).toHaveScreenshot('launch-nestopia.png')
+  })
+})
