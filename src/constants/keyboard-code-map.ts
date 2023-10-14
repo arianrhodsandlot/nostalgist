46a94260bab76f42bed4e6749a5c83e4895f86ab
Arianrhod Sandlot <theguidanceofawhitetower@gmail.com>
2023-10-14 16:38:18 +0800

    feat: add some new methods about pressing button programmatically

---
+++ src/constants/keyboard-code-map.ts
@@ -0,0 +1,49 @@
+export const keyboardCodeMap: Record<string, string> = {
+  left: 'ArrowLeft',
+  right: 'ArrowRight',
+  up: 'ArrowUp',
+  down: 'ArrowDown',
+  enter: 'Enter',
+  kp_enter: 'NumpadEnter',
+  tab: 'Tab',
+  insert: 'Insert',
+  del: 'Delete',
+  end: 'End',
+  home: 'Home',
+  shift: 'ShiftLeft',
+  rshift: 'ShiftRight',
+  ctrl: 'ControlLeft',
+  alt: 'AltLeft',
+  space: 'Space',
+  escape: 'Escape',
+  add: 'NumpadAdd', // is this right?
+  subtract: 'NumpadSubtract', // is this right?
+  kp_plus: 'NumpadAdd',
+  kp_minus: 'NumpadSubtract',
+  pageup: 'PageUp',
+  pagedown: 'PageDown',
+  period: 'Period',
+  capslock: 'CapsLock',
+  numlock: 'NumLock',
+  backspace: 'Backspace',
+  multiply: 'NumpadMultiply',
+  divide: 'NumpadDivide',
+  print_screen: 'PrintScreen',
+  scroll_lock: 'ScrollLock',
+  tilde: '', // what's this?
+  backquote: 'Backquote',
+  pause: 'Pause',
+  quote: 'Quote',
+  comma: 'Comma',
+  minus: 'Minus',
+  slash: 'Slash',
+  semicolon: 'Semicolon',
+  equals: 'Equal',
+  leftbracket: 'BracketLeft', // is this right?
+  backslash: '', // what's this?
+  rightbracket: 'BracketRight', // is this right?
+  kp_period: 'NumpadDecimal',
+  kp_equals: 'NumpadEquals', // is this right?
+  rctrl: 'ControlRight',
+  ralt: 'AltRight',
+}
---
+++ src/constants/system.ts
@@ -0,0 +1,8 @@
+export const systemCoreMap: Record<string, string> = {
+  gb: 'mgba',
+  gba: 'mgba',
+  gbc: 'mgba',
+  megadrive: 'genesis_plus_gx',
+  nes: 'fceumm',
+  snes: 'snes9x',
+}
