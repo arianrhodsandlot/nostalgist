3bcfe0beb1f581a5d250794db27171dae8fc1613
Arianrhod Sandlot <theguidanceofawhitetower@gmail.com>
2024-07-24 13:53:01 +0800

    test: update playwright config

--- tests/e2e/playwright.config.ts
+++ tests/e2e/playwright.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from '@playwright/test'
 import isCi from 'is-ci'
 
 export default defineConfig({
-  expect: { toHaveScreenshot: { maxDiffPixels: 100 } },
+  expect: { toHaveScreenshot: { maxDiffPixels: 300 } },
   fullyParallel: true,
   reporter: 'html',
   retries: 5,
